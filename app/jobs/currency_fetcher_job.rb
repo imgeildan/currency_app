6f7728b14679edc098318f9e68fe069167a4ef62
imgeildan <balerimge@gmail.com>
2022-04-22 11:53:36 +0200

    added methods into perform

--- app/jobs/currency_fetcher_job.rb
+++ app/jobs/currency_fetcher_job.rb
@@ -2,6 +2,11 @@ class CurrencyFetcherJob
 	@queue = :default
 
 	def self.perform
+		response = get_currencies_from_api
+		create_or_update_currencies(response['quotes']) if parsed_response['success']
+	end
+
+	def self.get_currencies_from_api
 		api_url  		   = 'http://api.currencylayer.com/live'
 		allowed_currencies = Currency::ALLOWED_CURRENCIES.join(',')
 		access_key         = Rails.application.credentials.money[:access_key]
@@ -9,18 +14,17 @@ class CurrencyFetcherJob
 		uri 			   = URI.parse(url)
 		request 		   = Net::HTTP::Get.new(uri)
 		response 		   = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(request) }
-		parsed_response    = JSON.parse(response.body)
-		currencies_hash	   = parsed_response['quotes']
+		JSON.parse(response.body)
+	end
 
-		if parsed_response['success']
-			currencies_hash.each do |curr_name, curr_rate|
-				name 	 = curr_name[3..6] # 'USDEUR' --> 'EUR'
-				currency = Currency.find_by(name: name)
-				if currency.present?
-					currency.update(rate: curr_rate) if curr_rate != currency.rate
-				else
-					Currency.create!(name: name, rate: curr_rate)
-				end
+	def self.create_or_update_currencies(currencies_hash)
+		currencies_hash.each do |curr_name, curr_rate|
+			name 	 = curr_name[3..6] # 'USDEUR' --> 'EUR'
+			currency = Currency.find_by(name: name)
+			if currency.present?
+				currency.update(rate: curr_rate) if curr_rate != currency.rate
+			else
+				Currency.create!(name: name, rate: curr_rate)
 			end
 		end
 	end
